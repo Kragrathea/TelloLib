604727d98ab23e0c7bc9970e209b38d6adcad535
Kragrathea <maildropdrop@gmail.com>
2018-05-04 16:17:21 -0700

    Working on layout.

--- aTello/Resources/Resource.Designer.cs
+++ aTello/Resources/Resource.Designer.cs
@@ -41,44 +41,75 @@ namespace aTello
 			}
 		}
 		
+		public partial class Drawable
+		{
+			
+			// aapt resource value: 0x7f020000
+			public const int land = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int land_white = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int takeoff = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int takeoff_white = 2130837507;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050005
-			public const int ac_state = 2131034117;
+			// aapt resource value: 0x7f060009
+			public const int ac_state = 2131099657;
 			
-			// aapt resource value: 0x7f05000a
-			public const int attAngleText = 2131034122;
+			// aapt resource value: 0x7f06000c
+			public const int attAngleText = 2131099660;
 			
-			// aapt resource value: 0x7f050003
-			public const int button1 = 2131034115;
+			// aapt resource value: 0x7f060007
+			public const int button1 = 2131099655;
 			
-			// aapt resource value: 0x7f050000
-			public const int connectButton = 2131034112;
+			// aapt resource value: 0x7f060000
+			public const int connectButton = 2131099648;
 			
-			// aapt resource value: 0x7f05000b
-			public const int eisText = 2131034123;
+			// aapt resource value: 0x7f06000d
+			public const int eisText = 2131099661;
 			
-			// aapt resource value: 0x7f050009
-			public const int exposureText = 2131034121;
+			// aapt resource value: 0x7f06000b
+			public const int exposureText = 2131099659;
 			
-			// aapt resource value: 0x7f050004
-			public const int joystick_state = 2131034116;
+			// aapt resource value: 0x7f060003
+			public const int imageButton1 = 2131099651;
 			
-			// aapt resource value: 0x7f050008
-			public const int maxHeightText = 2131034120;
+			// aapt resource value: 0x7f060008
+			public const int joystick_state = 2131099656;
 			
-			// aapt resource value: 0x7f050006
-			public const int relativeLayout1 = 2131034118;
+			// aapt resource value: 0x7f060004
+			public const int linearLayout1 = 2131099652;
 			
-			// aapt resource value: 0x7f050001
-			public const int surfaceView = 2131034113;
+			// aapt resource value: 0x7f06000a
+			public const int maxHeightText = 2131099658;
 			
-			// aapt resource value: 0x7f050002
-			public const int takeoffButton = 2131034114;
+			// aapt resource value: 0x7f060001
+			public const int relativeLayout1 = 2131099649;
 			
-			// aapt resource value: 0x7f050007
-			public const int textView1 = 2131034119;
+			// aapt resource value: 0x7f060002
+			public const int surfaceView = 2131099650;
+			
+			// aapt resource value: 0x7f060006
+			public const int takeoffButton = 2131099654;
+			
+			// aapt resource value: 0x7f060005
+			public const int textView1 = 2131099653;
 			
 			static Id()
 			{
@@ -93,11 +124,11 @@ namespace aTello
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int Main = 2130968576;
 			
-			// aapt resource value: 0x7f030001
-			public const int Settings = 2130903041;
+			// aapt resource value: 0x7f040001
+			public const int Settings = 2130968577;
 			
 			static Layout()
 			{
@@ -112,8 +143,8 @@ namespace aTello
 		public partial class Mipmap
 		{
 			
-			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			// aapt resource value: 0x7f030000
+			public const int Icon = 2130903040;
 			
 			static Mipmap()
 			{
@@ -128,17 +159,17 @@ namespace aTello
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
+			// aapt resource value: 0x7f050001
+			public const int app_name = 2131034113;
 			
-			// aapt resource value: 0x7f040000
-			public const int connect_status = 2130968576;
+			// aapt resource value: 0x7f050000
+			public const int connect_status = 2131034112;
 			
-			// aapt resource value: 0x7f040002
-			public const int statusArea = 2130968578;
+			// aapt resource value: 0x7f050002
+			public const int statusArea = 2131034114;
 			
-			// aapt resource value: 0x7f040003
-			public const int statusArea2 = 2130968579;
+			// aapt resource value: 0x7f050003
+			public const int statusArea2 = 2131034115;
 			
 			static String()
 			{
